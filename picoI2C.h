efc66398d62ed59c1d44127d074d6ebf6a9196b0
Nerd Ralph <ralphdoncaster@gmail.com>
2020-01-07 15:12:13 -0400

    bugfix in rw routine, reformat comments, uppercase class name

--- picoI2C.h
+++ picoI2C.h
@@ -1,7 +1,6 @@
 /* Small i2c master for AVR devices
- * Ralph Doncaster (c) 2020
+ * Ralph Doncaster (c) 2019
  * free software - MIT license
- * v 0.5 20200102
  */
 
 #pragma once
@@ -9,7 +8,6 @@
 #include <stdint.h>
 #include <avr/io.h>
 
-#define I2C_PORT PORTB 
 #define I2C_DDR DDRB
 #define I2C_PIN PINB
 
@@ -17,7 +15,7 @@ const int SUCCESS = 0;
 const int ADDR_NAK = 2;
 const int BUS_INUSE = 5;
 
-class picoI2C {
+class PicoI2C {
 public:
     static uint8_t startWrite(uint8_t addr)
     {
@@ -58,10 +56,10 @@ public:
     
     static void stop()
     {
-        I2C_DDR |= (1<<SDA);        // SDA low
-        I2C_DDR &= ~(1<<SCL);       // release SCL
+        I2C_DDR |= (1<<SDA);            // SDA low
+        I2C_DDR &= ~(1<<SCL);           // release SCL
         asm ("lpm" ::: "r0");           // 3 cycle delay
-        I2C_DDR &= ~(1<<SDA);       // release SDA
+        I2C_DDR &= ~(1<<SDA);           // release SDA
     }
     
     // ack !=0 to send ack
@@ -70,10 +68,10 @@ public:
         uint8_t data = rw(0xFF);
     
         if (ack)
-            I2C_DDR |= (1<<SDA);    // SDA low = ACK
+            I2C_DDR |= (1<<SDA);        // SDA low = ACK
         scl_hi();
         asm ("nop");                    // delay
-        I2C_DDR |= (1<<SCL);        // SCL low
+        I2C_DDR |= (1<<SCL);            // SCL low
     
         return data; 
     }
@@ -86,7 +84,7 @@ private:
             return BUS_INUSE;
         // set SDA to output mode for low 
         I2C_DDR |= (1<<SDA);
-        asm ("lpm" ::: "r0");       // 3 cycle delay
+        asm ("lpm" ::: "r0");           // 3 cycle delay
         // set SCL to output mode for low 
         I2C_DDR |= (1<<SCL);
         return 0;
@@ -107,23 +105,22 @@ private:
     {
         uint8_t bits = 8;
         do{
-            I2C_DDR &= ~(1<<SDA);   // SDA high
+            I2C_DDR &= ~(1<<SDA);       // SDA high
             if (!(data & 0x80)) I2C_DDR |= (1<<SDA);
             scl_hi();
             uint8_t i2c_pin = I2C_PIN;
-            I2C_DDR |= (1<<SCL);    // SCL low
+            I2C_DDR |= (1<<SCL);        // SCL low
+            data <<= 1;
             if (i2c_pin & (1 << SDA))
                 data |= 0x01;
-            data <<= 1;
         } while (--bits);
     
-        I2C_DDR &= ~(1<<SDA);       // release SDA
+        I2C_DDR &= ~(1<<SDA);           // release SDA
         return data;
     }
 
     // PORT pin numbers for SDA & SCL (0-7)
     static const uint8_t SCL;
     static const uint8_t SDA;
-
 };
 
